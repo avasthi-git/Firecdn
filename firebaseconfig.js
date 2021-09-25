7291a9843da689a492405d8962dd3df25ea2dc88
Ishan Avasthi <87573177+avasthi-git@users.noreply.github.com>
2021-09-25 16:20:07 +0530

    Update and rename firebase_config.js to firebaseconfig.js

---
+++ firebaseconfig.js
@@ -0,0 +1,16 @@
+/* Copy and Paste Firebase Config like below, Make sure it is named as "firebaseConfig" */
+
+/*---------------------------Replace------------------------------*/
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAic1PwlaKVfvvdxkKCDRLXoDdqWaW5qII",
+  authDomain: "ishanfirebase.firebaseapp.com",
+  projectId: "ishanfirebase",
+  storageBucket: "ishanfirebase.appspot.com",
+  messagingSenderId: "458183939701",
+  appId: "1:458183939701:web:9978347b7b3b3127e40a3c"
+
+/*---------------------------Replace------------------------------*/
+
+
+firebase.initializeApp(firebaseConfig); /* Initialize the Firebase*/
