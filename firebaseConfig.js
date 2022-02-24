bda4ac5d7320398cfc50163b83a3d124e8157edc
Ishan Avasthi <87573177+avasthi-git@users.noreply.github.com>
2022-02-24 17:21:10 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,13 +3,13 @@
 /*---------------------------Replace------------------------------*/
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAic1PwlaKVfvvdxkKCDRLXoDdqWaW5qII",
-  authDomain: "ishanfirebase.firebaseapp.com",
-  projectId: "ishanfirebase",
-  storageBucket: "ishanfirebase.appspot.com",
-  messagingSenderId: "458183939701",
-  appId: "1:458183939701:web:9978347b7b3b3127e40a3c",
-  measurementId: "G-1HN1R7QCVH"
+  apiKey: "AIzaSyDh_r2uaGTcv_KKsnT2hNOSG0rwZZvuSKo",
+  authDomain: "ifirecdn.firebaseapp.com",
+  projectId: "ifirecdn",
+  storageBucket: "ifirecdn.appspot.com",
+  messagingSenderId: "207572283555",
+  appId: "1:207572283555:web:8e8f1ebf03541baacb4f16",
+  measurementId: "G-5T903KKR87"
   };
 
 /*---------------------------Replace------------------------------*/
